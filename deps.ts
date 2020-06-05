4607398588d5226978db3f47fb51afdb63ab0221
Henrique Emanoel Viana <hv5088@gmail.com>
2020-06-05 11:02:22 -0300

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { ensureDir, move } from "https://deno.land/std@0.55.0/fs/mod.ts";
+export { ensureDir, ensureDirSync, move } from "https://deno.land/std@0.55.0/fs/mod.ts";
 export { v4 } from "https://deno.land/std@0.55.0/uuid/mod.ts";
 export { MultipartReader } from "https://deno.land/std@0.55.0/mime/multipart.ts";
