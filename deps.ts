b2d1ef0e6f0209e5961e10734e58e5aa29c940e0
Henrique Emanoel Viana <hv5088@gmail.com>
2022-02-27 10:31:46 -0300

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,5 @@
 export { ensureDir, ensureDirSync, move } from "https://deno.land/std@0.127.0/fs/mod.ts";
 export { v4 } from "https://deno.land/std@0.127.0/uuid/mod.ts";
 export { MultipartReader } from "https://deno.land/std@0.127.0/mime/multipart.ts";
+exort { SEP, join } from "https://deno.land/std@0.127.0/path/mod.ts";
+
